225fdfa97a38d08a507ea1d2aaa13a5a8a68c5e8
Aldovani <aldovane.etec@gmail.com>
2020-11-18 17:51:51 -0300

    Finalizando Projeto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
+import Contador from './components/contador'
 
 function App() {
-
-  
-
   return (
 
-    <Contador/>
+    <Contador />
+
   );
 }
 
 export default App;
 
 
-function Contador() {
-  // Declare uma nova variável de state, a qual chamaremos de "count"
-  const [contadornum, setcontador] = useState(0);
 
-  return (
-    <div>
-      <p>Você  clicou {contadornum} vezes</p>
-      <button onClick={() => setcontador(contadornum + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
+
